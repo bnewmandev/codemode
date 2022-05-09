3e054a1bbdecd57e35e835fca0051fa4bc905cdd
Ben Newman <ben@bndev.co.uk>
2022-05-09 21:24:25 +0100

    working on full plugin

---
+++ plugin/CodeMode/CodeMode/CodeModeRibbon.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace CodeMode
+{
+    partial class CodeModeRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public CodeModeRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CodeModeRibbon));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.codeMode = this.Factory.CreateRibbonGroup();
+            this.toggleCodeMode = this.Factory.CreateRibbonToggleButton();
+            this.selectCodeColor = this.Factory.CreateRibbonButton();
+            this.selectBackgroundColor = this.Factory.CreateRibbonButton();
+            this.codeFont = this.Factory.CreateRibbonComboBox();
+            this.tab1.SuspendLayout();
+            this.codeMode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.ControlId.OfficeId = "TabHome";
+            this.tab1.Groups.Add(this.codeMode);
+            this.tab1.Label = "TabHome";
+            this.tab1.Name = "tab1";
+            // 
+            // codeMode
+            // 
+            this.codeMode.Items.Add(this.toggleCodeMode);
+            this.codeMode.Items.Add(this.selectCodeColor);
+            this.codeMode.Items.Add(this.selectBackgroundColor);
+            this.codeMode.Items.Add(this.codeFont);
+            this.codeMode.Label = "Code Mode";
+            this.codeMode.Name = "codeMode";
+            // 
+            // toggleCodeMode
+            // 
+            this.toggleCodeMode.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.toggleCodeMode.Description = "Toggles Code Mode";
+            this.toggleCodeMode.Image = ((System.Drawing.Image)(resources.GetObject("toggleCodeMode.Image")));
+            this.toggleCodeMode.Label = "Toggle Code Mode";
+            this.toggleCodeMode.Name = "toggleCodeMode";
+            this.toggleCodeMode.ShowImage = true;
+            this.toggleCodeMode.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.toggleCodeMode_Click);
+            // 
+            // selectCodeColor
+            // 
+            this.selectCodeColor.Label = "Select Code Colour";
+            this.selectCodeColor.Name = "selectCodeColor";
+            this.selectCodeColor.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.selectCodeColor_Click);
+            // 
+            // selectBackgroundColor
+            // 
+            this.selectBackgroundColor.Label = "Select Background Colour";
+            this.selectBackgroundColor.Name = "selectBackgroundColor";
+            this.selectBackgroundColor.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.selectBackgroundColor_Click);
+            // 
+            // codeFont
+            // 
+            this.codeFont.Label = "Code Font";
+            this.codeFont.MaxLength = 20;
+            this.codeFont.Name = "codeFont";
+            this.codeFont.Text = null;
+            this.codeFont.TextChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.codeFont_TextChanged);
+            // 
+            // CodeModeRibbon
+            // 
+            this.Name = "CodeModeRibbon";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.CodeModeRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.codeMode.ResumeLayout(false);
+            this.codeMode.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup codeMode;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton toggleCodeMode;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton selectCodeColor;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton selectBackgroundColor;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox codeFont;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal CodeModeRibbon CodeModeRibbon
+        {
+            get { return this.GetRibbon<CodeModeRibbon>(); }
+        }
+    }
+}
---
+++ plugin/CodeMode/CodeMode/CodeModeRibbon.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualBasic;
+
+namespace CodeMode
+{
+
+    public class FontItem : RibbonDropDownItem
+    {
+        public string Label { get; set; }
+        public string Id { get; }
+        public Image Image { get; set; }
+        public string OfficeImageId { get; set; }
+        public OfficeRibbon Ribbon { get; }
+        public RibbonComponent Parent { get; }
+        public string ScreenTip { get; set; }
+        public string SuperTip { get; set; }
+        public object Tag { get; set; }
+    }
+
+    public partial class CodeModeRibbon
+    {
+        ColorDialog colorPicker;
+        static WdColor codeColor;
+        static WdColor bgColor;
+
+        private void CodeModeRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            foreach (FontFamily font in FontFamily.Families)
+            {
+
+                this.codeFont.Items.Add(new FontItem() { Label = font.Name, Tag = font.Name });
+            }
+
+        }
+
+        private void toggleCodeMode_Click(object sender, RibbonControlEventArgs e)
+        {
+            if (this.toggleCodeMode.Checked)
+            {
+                Selection initialLocation = Globals.ThisAddIn.Application.Selection;
+                ThisAddIn.initialLocation = initialLocation.Range;
+
+                string initialFont = initialLocation.Font.Name;
+                ThisAddIn.initialFont = initialFont;
+
+                initialLocation.Font.Name = "Courier New";
+
+
+                Debug.WriteLine(initialLocation.Start);
+            } else
+            {
+                Selection finalLocation = Globals.ThisAddIn.Application.Selection;
+                Range fullCodeRange = Globals.ThisAddIn.Application.ActiveDocument.Range(ThisAddIn.initialLocation.Start, finalLocation.End);
+                fullCodeRange.Select();
+                fullCodeRange.NoProofing = 1;
+                fullCodeRange.Shading.ForegroundPatternColor = WdColor.wdColorAutomatic;
+                fullCodeRange.Shading.BackgroundPatternColor = (WdColor)2829099;
+                Selection selection = Globals.ThisAddIn.Application.Selection;
+                selection.MoveRight();
+                selection.InsertAfter(" ");
+                selection.Font.Name = ThisAddIn.initialFont;
+                selection.Shading.BackgroundPatternColor = WdColor.wdColorAutomatic;
+                selection.MoveRight();
+            }
+        }
+
+
+        private void selectCodeColor_Click(object sender, RibbonControlEventArgs e)
+        {
+            colorPicker = new ColorDialog();
+            colorPicker.AllowFullOpen = true;
+            colorPicker.ShowHelp = true;
+            colorPicker.ShowDialog();
+
+            string colorString = colorPicker.Color.R.ToString() + colorPicker.Color.G.ToString() + colorPicker.Color.B.ToString();
+            codeColor = (WdColor)Int32.Parse(colorString);
+
+        }
+
+        private void selectBackgroundColor_Click(object sender, RibbonControlEventArgs e)
+        {
+            colorPicker = new ColorDialog();
+            colorPicker.AllowFullOpen = true;
+            colorPicker.ShowHelp = true;
+            colorPicker.ShowDialog();
+
+            string colorString = colorPicker.Color.R.ToString() + colorPicker.Color.G.ToString() + colorPicker.Color.B.ToString();
+            bgColor = (WdColor)Int32.Parse(colorString);
+        }
+
+        private void codeFont_TextChanged(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
